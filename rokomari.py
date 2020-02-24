0ac533d7e9fc68d3ca0f4b90b389168277d2d9d5
Ikhtiar Ahmed <32925985+ikhtiar24@users.noreply.github.com>
2020-02-24 14:08:17 +0600

    Add files via upload

---
+++ rokomari.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import csv
+
+url = 'http://www.rokomari.com/book/category/2407/book-fair-2020-?sort=&page=1'
+
+res = requests.get(url).text
+try:
+    bookContent = BeautifulSoup(res, 'html.parser')
+    pagination = bookContent.find("div", class_='pagination')
+    totalPage = pagination.find_all('a')
+    page = int(totalPage[len(totalPage) - 2].text)
+    # print(page)
+
+    for x in range(page + 1):
+        # manipulate link
+        sourceCode = requests.get(url + str(x + 1)).text
+
+        # BeautifulSoup object
+        soup = BeautifulSoup(sourceCode, 'html.parser')
+
+        # Collect everything from <div> tags with class <book-list-wrapper>
+        for eachBook in soup.find_all('div', {'class': 'book-list-wrapper'}):
+            content = []
+            bookTitle = eachBook.find('p', {'class': 'book-title'}).text
+            bookAuthor = eachBook.find('p', {'class': 'book-author'}).text
+            bookStatus = eachBook.find('p', {'class': 'book-status'}).text
+            try:
+                bookOrgPrice = eachBook.find('p', {'class': 'book-price'}).strike.text
+            except:
+                bookOrgPrice = ''
+            bookPrice = eachBook.find('p', {'class': 'book-price'}).span.text
+
+            content.append([bookTitle, bookAuthor, bookStatus, bookOrgPrice, bookPrice])
+            print(content)
+           
+            with open("rokomari_book_fair_2020.csv", 'a', newline='', encoding='utf-8') as csvfile:
+                csvfile.write('\ufeff')
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
+                writer.writerows(content)
+except Exception as e:
+    print(e)
